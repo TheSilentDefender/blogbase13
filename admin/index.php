1cf40f6c9beb6b04434086e6e9f106ca721bf380
Madhav Kapa <madhavkapa16@gmail.com>
2020-07-25 16:18:06 +0530

    aaaaaaaaaaaaaaa

--- admin/index.php
+++ admin/index.php
@@ -1,91 +1,12 @@
 <?php
-#===============================================================================
-# DEFINE: Administration
-#===============================================================================
-define('ADMINISTRATION', TRUE);
-define('AUTHENTICATION', TRUE);
+session_start();
 
-#===============================================================================
-# INCLUDE: Initialization
-#===============================================================================
-require '../core/application.php';
+include('../config/config.php');
+include('../lib/app.lib.php');
 
-#===============================================================================
-# TRY: PDOException
-#===============================================================================
-try {
-	$execute = 'SELECT id FROM %s ORDER BY time_insert DESC LIMIT 1';
+userIsConnected();
 
-	$LastPageStatement = $Database->query(sprintf($execute, Page\Attribute::TABLE));
-	$LastPostStatement = $Database->query(sprintf($execute, Post\Attribute::TABLE));
-	$LastUserStatement = $Database->query(sprintf($execute, User\Attribute::TABLE));
+$vue='home';
+$title = 'Admin - Bienvenue';
 
-	$execute = 'SELECT COUNT(*) FROM %s';
-
-	$PageCountStatement = $Database->query(sprintf($execute, Page\Attribute::TABLE));
-	$PostCountStatement = $Database->query(sprintf($execute, Post\Attribute::TABLE));
-	$UserCountStatement = $Database->query(sprintf($execute, User\Attribute::TABLE));
-}
-
-#===============================================================================
-# CATCH: PDOException
-#===============================================================================
-catch(PDOException $Exception) {
-	exit($Exception->getMessage());
-}
-
-#===============================================================================
-# TRY: Template\Exception
-#===============================================================================
-try {
-	try {
-		$LastPage = Page\Factory::build($LastPageStatement->fetchColumn());
-		$LastPageUser = User\Factory::build($LastPage->attr('user'));
-
-		$PageItemTemplate = generatePageItemTemplate($LastPage, $LastPageUser);
-	}
-
-	catch(Page\Exception $Exception){}
-	catch(User\Exception $Exception){}
-
-	try {
-		$LastPost = Post\Factory::build($LastPostStatement->fetchColumn());
-		$LastPostUser = User\Factory::build($LastPost->attr('user'));
-
-		$PostItemTemplate = generatePostItemTemplate($LastPost, $LastPostUser);
-	}
-
-	catch(Post\Exception $Exception){}
-	catch(User\Exception $Exception){}
-
-	try {
-		$LastUser = User\Factory::build($LastUserStatement->fetchColumn());
-		$UserItemTemplate = generateUserItemTemplate($LastUser);
-	} catch(User\Exception $Exception){}
-
-	$HomeTemplate = Template\Factory::build('home');
-	$HomeTemplate->set('LAST', [
-		'PAGE' => $PageItemTemplate ?? FALSE,
-		'POST' => $PostItemTemplate ?? FALSE,
-		'USER' => $UserItemTemplate ?? FALSE,
-
-	]);
-
-	$HomeTemplate->set('COUNT', [
-		'PAGE' => $PageCountStatement->fetchColumn(),
-		'POST' => $PostCountStatement->fetchColumn(),
-		'USER' => $UserCountStatement->fetchColumn(),
-	]);
-
-	$MainTemplate = Template\Factory::build('main');
-	$MainTemplate->set('NAME', 'Dashboard');
-	$MainTemplate->set('HTML', $HomeTemplate);
-	echo $MainTemplate;
-}
-
-#===============================================================================
-# CATCH: Template\Exception
-#===============================================================================
-catch(Template\Exception $Exception) {
-	Application::exit($Exception->getMessage());
-}
+include('tpl/layout.phtml');
--- core/namespace/User/Exception.php
+++
@@ -1,5 +0,0 @@
-<?php
-namespace User;
-
-class Exception extends \Exception {}
-?>
