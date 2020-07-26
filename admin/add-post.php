d1be119f24cf64cf1d279ab0248d62603afddaae
Madhav Kapa <madhavkapa16@gmail.com>
2020-07-26 15:10:58 +0530

    aaaaaaaaaaa

--- admin/add-post.php
+++ admin/add-post.php
@@ -32,12 +32,7 @@ else
 //rename the image file
 $imgnewfile=md5($imgfile).$extension;
 // Code for move image into directory
-function fileUpload () {
-    $target_dir = "postimage/";
-    $file_name = $_FILES['postimage']['tmp_name'];
-    $file_tmp = $_FILES['csv_file']['tmp_name'];
-}
-//move_uploaded_file($_FILES["postimage"]["tmp_name"],"postimages/".$imgnewfile);
+move_uploaded_file($_FILES["postimage"]["tmp_name"],"postimages/".$imgnewfile);
 
 $status=1;
 $query=mysqli_query($con,"insert into tblposts(PostTitle,CategoryId,SubCategoryId,PostDetails,PostUrl,Is_Active,PostImage) values('$posttitle','$catid','$subcatid','$postdetails','$url','$status','$imgnewfile')");
